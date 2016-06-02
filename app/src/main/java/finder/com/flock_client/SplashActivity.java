0e72b5056fa10c119f42b31c572053676f2bf51d
Arthelon <hsing.daniel@gmail.com>
2016-06-02 20:13:36 +0800

    Added boilerplate auth activity and splash screen

--- app/src/main/java/finder/com/flock_client/SplashActivity.java
+++ app/src/main/java/finder/com/flock_client/SplashActivity.java
@@ -1,5 +1,6 @@
 package finder.com.flock_client;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(this, AuthActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
